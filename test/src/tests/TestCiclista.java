2187c7c1b1256114bce3b36f1651fe8558c95c91
Plandevida <plandevida@planes.com>
2013-01-03 17:14:59 +0100

    tests

---
+++ test/src/tests/TestCiclista.java
@@ -0,0 +1,17 @@
+package src.tests;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class TestCiclista {
+	
+	@Test
+	public void test() {
+		assertEquals("Result", 50, 50);
+	}
+	
+}
